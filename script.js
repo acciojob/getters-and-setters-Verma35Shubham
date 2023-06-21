81386d79660d2d67d6e88c721cfdbffd01ed3b21
Shubham Verma <verma.shubham2537@gmail.com>
2023-06-21 16:33:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,34 @@
-//complete this code
 class Person {
-    constructor(name,age) {
-        this.name = name;
-		this.age = age
-    }
-    getName() {
-        return this.name;
-    }
-    setAge(newAge) {
-        this.age = newAge;
-        }
-        
-    
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set age(age) {
+    this._age = age;
+  }
 }
 
 class Student extends Person {
-	study(){
-		console.log(`${this.name} is studying.`)
-	}
+  study() {
+    console.log(`${this._name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(`${this.name} is teaching.`)
-	}
+  teach() {
+    console.log(`${this._name} is teaching`);
+  }
 }
 
-
-let student = new Student("Jhon", 30)
-student.study()
-// let student1 = new Student("Alice", 30)
-// student1.study()
-let teacher = new Teacher("Alice", 30)
-teacher.teach()
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
 
 // Do not change the code below this line
 window.Person = Person;
